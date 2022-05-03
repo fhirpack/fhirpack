84f05c1a8ab17202a6dbe49dc77be0c21faab9ab
tillrostalski <mr453@uni-heidelberg.de>
2022-05-03 18:06:46 +0200

    deleted decorator

--- src/fhirdrill/base.py
+++ src/fhirdrill/base.py
@@ -70,7 +70,6 @@ class BaseMixin:
 
         return output
 
-    # @decorators.validateFrame
     def prepareOutput(self, data, resourceType=None, columns=["data"], wrap=True):
 
         if len(data) and not resourceType:
