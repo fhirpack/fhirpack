982fda34d89fe61d0419d0f66ee559c8182225da
tillrostalski <mr453@uni-heidelberg.de>
2022-05-03 18:11:35 +0200

    opimized flattenList

--- src/fhirdrill/exceptions.py
+++ src/fhirdrill/exceptions.py
@@ -1,5 +1,5 @@
-import json
 from enum import Enum
+import requests
 
 
 class BaseError(Exception):
@@ -15,3 +15,32 @@ class ErrorSeverity(Enum):
     error = "error"
     warning = "warning"
     information = "information"
+
+
+class ServerConnectionException(requests.exceptions.MissingSchema):
+    """Raised when drill does not have connection to server."""
+
+    pass
+
+
+class InvalidInputDataException(ValueError):
+    """Raised when invalid input types are passed."""
+
+    pass
+
+
+class InvalidSearchParams(ValueError):
+    """Raised when invalid key is used for searchParams"""
+
+    pass
+
+
+class InvalidOperationException:
+    """Raised when operation is invalid for data."""
+
+    pass
+
+
+class InvalidConfigurationException:
+
+    pass
