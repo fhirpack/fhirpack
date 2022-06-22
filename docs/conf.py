73fe26423a701da3cb2f2e00616cdc54cf45f3a4
tillrostalski <mr453@uni-heidelberg.de>
2022-06-22 21:14:30 +0200

    fixed not included files warning

--- docs/conf.py
+++ docs/conf.py
@@ -146,7 +146,7 @@ release = version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".venv", "_test", "test_"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".venv", "_test", "test_", "api/modules.rst", "api/fhirpack.rst", "api/fhirpack.custom*"]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 # default_role = None
