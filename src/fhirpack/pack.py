46484a0755ba0a3e9d066a0883fbbb58c4cd1fa6
Till Rostalski <rostis@hotmail.de>
2022-07-08 05:59:54 +0000

    Update pack.py

--- src/fhirpack/pack.py
+++ src/fhirpack/pack.py
@@ -55,7 +55,6 @@ class PACK(
             warnings.warn("PACK is not connected to server.")
             self.logger.info("PACK is not connected to server.")
             self.client = SyncFHIRClient("")
-            # self.client = None
 
         self.logger.info("pack initialization finished")
 
